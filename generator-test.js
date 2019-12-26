14a428fe7cc72e50992902ead0aa205be2aaf32d
monkeyDugi <lbd4946@gmail.com>
2019-12-26 22:37:30 +0900

    제너레이터

--- generator-test.js
+++ generator-test.js
@@ -1,11 +1,11 @@
 // 제너레이터 함수 정의
 function * counter () {
-    yield 1
-    yield 2
-    yield 3
+  yield 1
+  yield 2
+  yield 3
 }
 // 제너레이터 객체 생성
-const g = counter();
+const g = counter()
 // next() 메서드 호출
 console.log(g.next())
 console.log(g.next())
