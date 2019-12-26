7a51d69bf97ff006f26ea89de363d0e7cbc0b459
monkeyDugi <lbd4946@gmail.com>
2019-12-26 21:18:10 +0900

    node.js로 웹 서버 구현

---
+++ hello-server.js
@@ -0,0 +1,18 @@
+// http 모듈 읽기
+const http = require('http')
+
+// 웹 서버 실행
+const svr = http.createServer(handler) // 서버 생성
+svr.listen(8081) // 8081 포트 사용
+
+// 서버에 접근이 생기면 처리
+function handler (req, res) {
+    console.log('url :', req.url)
+    console.log('method :', req.method)
+
+    // HTTP 헤더 출력
+    res.writeHead(200, {'Content-Type': 'text/html'})
+
+    // 응답 본문 출력
+    res.end('<h1>Hello, World!</h1>\n')
+}
