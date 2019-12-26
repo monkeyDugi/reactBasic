6f4ccb987119416960f159dffbeba6979a1146d6
monkeyDugi <lbd4946@gmail.com>
2019-12-26 22:36:53 +0900

    node.js로 웹 서버 구현

--- hello-server.js
+++ hello-server.js
@@ -7,12 +7,12 @@ svr.listen(8081) // 8081 포트 사용
 
 // 서버에 접근이 생기면 처리
 function handler (req, res) {
-    console.log('url :', req.url)
-    console.log('method :', req.method)
+  console.log('url :', req.url)
+  console.log('method :', req.method)
 
-    // HTTP 헤더 출력
-    res.writeHead(200, {'Content-Type': 'text/html'})
+  // HTTP 헤더 출력
+  res.writeHead(200, { 'Content-Type': 'text/html' })
 
-    // 응답 본문 출력
-    res.end('<h1>Hello, World!</h1>\n')
+  // 응답 본문 출력
+  res.end('<h1>Hello, World!</h1>\n')
 }
